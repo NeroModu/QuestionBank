cfb1b6424265966677582d564e57988a5a6c639a
Enrique <edavalos@ucsc.edu>
2020-11-09 01:36:57 -0800

    Create Solution.py

---
+++ LeetCode/7. Reverse Integer/Solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        rev = 0
+        
+        neg = False
+        if x < 0:
+            neg = True
+            x *= -1
+            
+        while x > 0:
+            a = x % 10
+            rev = rev * 10 + a
+            x = x // 10
+            
+        if abs(rev) >= 2 ** 31 - 1:
+            return 0
+        
+        return rev if not neg else rev * -1
